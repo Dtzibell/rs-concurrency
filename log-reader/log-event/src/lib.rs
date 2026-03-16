7f7a5f5c5dd07b88849612b4402c27ad03a498ed
Dtzibell <zaleniakas.tauras@gmail.com>
2026-03-16 22:37:39 +0100

    sequential json parsing

--- log-reader/log-event/src/lib.rs
+++ log-reader/log-event/src/lib.rs
@@ -3,28 +3,35 @@ use serde::{Serialize, Deserialize};
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct LogEvent {
-    ts: DateTime<Local>,
-    level: String,
-    service: String,
-    latency: usize,
-    endpoint: String,
+    pub ts: DateTime<Local>,
+    pub level: String,
+    pub service: String,
+    pub latency: u64,
+    pub endpoint: String,
 }
 
 #[derive(Debug)]
 pub struct Stats {
-    entries: usize,
-    errors: usize,
-    fatals: usize,
-    average_latency: usize,
+    pub entries: u64,
+    pub errors: usize,
+    pub fatals: usize,
+    pub total_latency: u64,
+    pub average_latency: f64,
+}
+
+impl Stats {
+    pub fn new() -> Stats {
+        Stats {
+            entries: 0,
+            errors: 0,
+            fatals: 0,
+            total_latency: 0,
+            average_latency: 0.0,
+        }
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
 }
--- log-reader/log-read/src/main.rs
+++ log-reader/log-read/src/main.rs
@@ -1,10 +1,11 @@
 use clap::Parser;
+use std::collections::HashMap;
 use std::process;
 use std::path::PathBuf;
 use std::fs::File;
 use std::error::Error;
 use std::io::{BufReader, BufRead};
-use log_event::LogEvent;
+use log_event::{LogEvent, Stats};
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about=None)]
@@ -20,12 +21,32 @@ fn main() -> Result<(), Box<dyn Error>>{
         process::exit(1);
     }
     let mut reader = BufReader::new(File::open(path)?);
-    for i in 0..10 {
-        let mut buffer = String::new();
-        reader.read_line(&mut buffer)?;
-        println!("{buffer}");
+    let mut buffer = String::new();
+    let mut i = 0;
+    let mut data = HashMap::new();
+    'top: while let Ok(bytes) = reader.read_line(&mut buffer) {
+        if bytes == 0 { break 'top; }
+        if i % 25000 == 0 {
+            println!("{i}");
+        }
         let le: LogEvent = serde_json::from_str(&buffer)?;
-        println!("{:#?}", le);
+        let stats = data.entry(le.service).or_insert(Stats::new());
+        stats.total_latency += le.latency;
+        // println!("Service: {:#?}, Average = {}, Instance = {}", stats, stats.average_latency, le.latency);
+        stats.entries += 1;
+        if le.level == "error" {
+            stats.errors += 1;
+        }
+        if le.level == "fatal" {
+            stats.fatals += 1;
+        }
+
+        i += 1;
+        buffer.clear();
+    } 
+    for (key, value) in &mut data {
+        value.average_latency = value.total_latency as f64 / value.entries as f64;
     }
+    println!("{data:#?}");
     Ok(())
 }
