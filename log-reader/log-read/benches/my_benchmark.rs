a85e860e87d2e1cae290a604f076722fbcf02852
Dtzibell <zaleniakas.tauras@gmail.com>
2026-03-22 16:01:56 +0100

    thread argument for benching

--- log-reader/log-read/benches/my_benchmark.rs
+++ log-reader/log-read/benches/my_benchmark.rs
@@ -7,7 +7,7 @@ fn criterion_benchmark(c: &mut Criterion) {
     let mut group = c.benchmark_group("sample_size 10");
     group.sample_size(10);
     group.bench_function("main", |b| {
-        b.iter(|| read_data(black_box(Path::new("/home/dtzi/rs-conc/log1.json"))))
+        b.iter(|| read_data(black_box(Path::new("/home/dtzi/rs-conc/log1.json")), black_box(3)))
     });
 }
 
